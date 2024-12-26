f01ff8e863172d7f9dcdeb767f3ec52850ea543f
xlibnui_makix <103821356+ZastavnyeMishaUwU@users.noreply.github.com>
2024-12-26 16:43:48 +0200

    Add project files.

---
+++ dz11_12-1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace dz11_12_1
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write(" число словами (від zero до nine): ");
+            string input = Console.ReadLine();
+
+            try
+            {
+                int number = WordToNumber(input);
+                Console.WriteLine($"Цифра: {number}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
+        }
+
+        static int WordToNumber(string word)
+        {
+            switch (word.ToLower())
+            {
+                case "zero": return 0;
+                case "one": return 1;
+                case "two": return 2;
+                case "three": return 3;
+                case "four": return 4;
+                case "five": return 5;
+                case "six": return 6;
+                case "seven": return 7;
+                case "eight": return 8;
+                case "nine": return 9;
+                default:
+                    throw new ArgumentException("некоректне слово.");
+            }
+        }
+    }
+}
